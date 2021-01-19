0f9cfebf8371a60a0b29fc7e9f63a28a0ea6cce1
doggo42 <awesomev@outlook.com>
2021-01-19 18:28:56 -0500

    initial commit

---
+++ awall.py
@@ -0,0 +1,41 @@
+import discord
+import requests
+import json
+import re
+
+client = discord.Client()
+channels = []
+roles = []
+token = None
+
+@client.event
+async def on_ready():
+	print('ready')
+
+@client.event
+async def on_message(message):
+	if message.author == client.user or str(message.channel.id) not in channels or str(message.author.id) in roles:
+		return
+
+	msg = message.content.replace('```','')
+	blocks=re.findall(r"```([\w\W]+?)```",message.content)
+	for i in range(len(blocks)):
+		if blocks[i].count('\n') >= 14:
+			ext='.'+blocks[i][:blocks[i].find('\n')]
+			ext='' if ext == '.' else ext
+			req = requests.post('https://pastecord.com/documents',data=blocks[i])
+			msg = msg.replace(blocks[i],'https://pastecord.com/'+req.json()['key']+ext)
+	if msg != message.content:
+		await message.delete()
+		await message.channel.send(message.author.mention+' said:\n'+msg)
+
+with open('channels.txt') as f:
+	channels=f.read().splitlines()
+
+with open('whitelist.txt') as f:
+	roles=f.read().splitlines()
+
+with open('token.txt') as f:
+	token=f.read()
+
+client.run(token)
