c3224a3f3b13a5812d9546950140b531b5078a42
doggo42 <awesomev@outlook.com>
2021-05-21 10:02:17 -0400

    testing code detection

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,14 @@
 FROM doggo4242/pastef:latest
-COPY token.txt /etc/pastef/token.txt
 COPY whitelist.txt /etc/pastef/whitelist.txt
 COPY channels.json /etc/pastef/channels.json
+COPY requirements.txt /etc/pastef/requirements.txt
+COPY token.txt /etc/pastef/token.txt
 COPY pastef.py /usr/local/bin/pastef
 COPY formatter.py /usr/local/bin/formatter.py
-CMD ["pastef"]
+RUN apk update
+RUN apk --no-cache upgrade
+RUN apk --no-cache add gcc python3-dev musl-dev
+RUN python3 -m pip install --upgrade --no-cache-dir pip
+RUN python3 -m pip install --no-cache-dir -r /etc/pastef/requirements.txt
+RUN apk del gcc python3-dev musl-dev
+ENTRYPOINT ["pastef"]
--- pastef.py
+++ pastef.py
@@ -1,58 +1,93 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 import discord
-import requests
+from discord.ext import commands
+#import requests
+import aiohttp
 import json
 import re
+import os
 import formatter
 
-client = discord.Client()
-channels = {}
-token = None
-roles = []
-bin = b'\xf0\x9f\x93\x8e'
-format = [b'\xe2\x9c\x92\xef\xb8\x8f',b'\xf0\x9f\x96\x8b\xef\xb8\x8f',b'\xf0\x9f\x96\x8a\xef\xb8\x8f']
-lastMsgs = {}
+bot = commands.Bot(command_prefix='p!')
 
-@client.event
-async def on_ready():
-	print('ready')
+class Format(commands.Cog):
+	def __init__(self,bot,channels,roles):
+		self.bot = bot
+		self.channels = channels
+		self.roles = roles
 
-@client.event
-async def on_reaction_add(reaction,user):
-	embytes = reaction.emoji.encode('utf-8')
-	if str(reaction.message.channel.id) not in channels or bool(set(roles) & set(reaction.message.author.roles)) or embytes not in [bin]+format:
-		return
+		# utf-8 encoding of emotes
+		self.bin = b'\xf0\x9f\x93\x8e'
+		self.format = [b'\xe2\x9c\x92\xef\xb8\x8f',b'\xf0\x9f\x96\x8b\xef\xb8\x8f',b'\xf0\x9f\x96\x8a\xef\xb8\x8f']
+		self.lastMsgs = {}
 
-	await reaction.remove(user)
-	if len(lastMsgs) >= 200:
-		del lastMsgs[next(iter(lastMsgs.keys()))]
-	if lastMsgs.get(str(reaction.message.id)) != None:
-		last = lastMsgs.get(str(reaction.message.id))
-		if (last[0] and last[1]) or not (embytes == bin or last[0]) or not (embytes in format or last[1]):
+		self.keywords = [';','=','//','#','(',')','{','}',':','<','>','[',']','return',
+		'import','export','struct','class','interface','end','type','::','module',
+		'++','--','->','<-']
+
+	@commands.Cog.listener()
+	async def on_reaction_add(self,reaction,user):
+		embytes = reaction.emoji.encode('utf-8')
+		# if not in specified channels, user is exempt, or emoji is not paperclip or pen, return
+		if str(reaction.message.channel.id) not in self.channels or bool(set(self.roles) & set(reaction.message.author.roles)) or embytes not in [self.bin]+self.format:
 			return
-		lastMsgs[str(reaction.message.id)] = [(embytes == bin or last[0]),(embytes in format or last[1])]
-	else:
-		lastMsgs[str(reaction.message.id)] = [(embytes == bin),(embytes in format)]
 
-	msg = []
-	blocks=re.findall(r"```([\w\W]+?)```",reaction.message.content)
-	for block in blocks:
-		ext='.'+block[:block.find('\n')]
-		ext=channels[str(reaction.message.channel.id)] if ext == '.' else ext
-		formatted=block[block.find('\n')+1:]
-		if embytes != bin:#check for paperclip in which case formatter is not run
-			formatted=formatter.format(formatted,ext[1:]) if ext != '' else formatted
-		req = requests.post('https://pastecord.com/documents',data=formatted)
-		msg.append('https://pastecord.com/'+req.json()['key']+ext+'\n')
-	await reaction.message.reply(''.join(msg),mention_author=False)
+		await reaction.remove(user)
+		if len(self.lastMsgs) >= 200:
+			del self.lastMsgs[next(iter(self.lastMsgs.keys()))]
+		if self.lastMsgs.get(str(reaction.message.id)) is not None:
+			last = self.lastMsgs.get(str(reaction.message.id))
+			if (last[0] and last[1]) or not (embytes == self.bin or last[0]) or not (embytes in self.format or last[1]):
+				return
+			self.lastMsgs[str(reaction.message.id)] = ((embytes == self.bin or last[0]),(embytes in self.format or last[1]))
+		else:
+			self.lastMsgs[str(reaction.message.id)] = ((embytes == self.bin),(embytes in self.format))
 
+		msg = []
+		blocks=re.findall(r"```([\w\W]+?)```",reaction.message.content)
+
+		# extension needs fixing here:
+		if not blocks:
+			lines = reaction.message.content.splitlines()
+			for line in lines:
+				if any(i in line for i in self.keywords):
+					blocks.append(line)
+			if not blocks:
+				await reaction.message.reply('Could not identify code. Please use a codeblock. See ++wrapmini for more info.',mention_author=False)
+				return
+
+		for block in blocks:
+			# get language specifier for file extension & formatting
+			ext=block[:block.find('\n')]
+			ext=self.channels[str(reaction.message.channel.id)] if not ext else ext
+			formatted=block[block.find('\n')+1:]
+			if embytes != self.bin:
+				formatted=formatter.format(formatted,ext) if ext else formatted
+			async with aiohttp.ClientSession() as session:
+				async with session.post('https://pastecord.com/documents',data=formatted) as res:
+					lnk = await res.json()['key']
+					msg.append(f'https://pastecord.com/{lnk}.{ext}')
+#			req = requests.post('https://pastecord.com/documents',data=formatted)
+
+		await reaction.message.reply('\n'.join(msg),mention_author=False)
+
+# in progress
+	@commands.command()
+	async def format(ctx,start=0,end):
+		if ctx.message.reference is None:
+			await ctx.reply('Reply to the message you want to format.',mention_author=False)
+			return
+
+# read in config data & start
+channels = None
 with open('/etc/pastef/channels.json') as f:
-	channels=json.loads(f.read())
+	channels=json.load(f)
 
+roles = None
 with open('/etc/pastef/whitelist.txt') as f:
 	roles=f.read().splitlines()
 
-with open('/etc/pastef/token.txt') as f:
-	token=f.read()
+token=os.getenv('TOKEN')
 
-client.run(token)
+bot.add_cog(Format(bot,channels,roles))
+bot.run(token)
